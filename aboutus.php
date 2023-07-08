4868b38211834c880c1d36e8274c84e9e74e92f2
janhvi2306 <138986785+janhvi2306@users.noreply.github.com>
2023-07-08 19:24:29 +0530

    Add files via upload

---
+++ aboutus.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Booking management system</title>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+    <link rel="stylesheet" href="home2.css">
+    <link rel="stylesheet" href="home1.css">
+    
+</head>
+<body>
+    <header class="header">
+        <img src="newlogo.png" class="logo">
+      <h1>Wanderu</h1>
+        <form action="" class="search-form">
+            <input type="search" name="" placeholder="search here..." id="searchBox">
+            <label for="searchBox" class="fas fa-search"></label>
+        </form>
+        <nav>
+            <ul>
+               <li>
+                  <a href="index.php" style="font-size:20px;">Home </a>
+               </li>
+                  </ul>
+         </nav>
+        <div class="icons">
+            <div class="fas fa-search" id="search-btn"></div>
+            <!-- <div class="fas fa-moon" id="theme-btn"></div> -->
+            <!-- <div class="fas fa-user" id="login-btn">
+            </div> -->
+            <div class="fas fa-bars" id="menu-btn"></div>
+        </div>
+       
+
+        <nav class="navbar">
+            <a href="index.php">Home</a>
+            <a href="aboutus.php">About us</a>
+            <a href="contactus.php">Contact us</a>
+            <a href="#blogs">Blog</a>
+        </nav>
+        <!-- <nav class="navbar1">
+                    <a href="#">User Login</a>
+                    <a href="#">Admin Login</a>
+       </nav> -->
+</header><br><br><br><br>
+
+    <div class="section">
+		<div class="container">
+			<div class="content-section">
+				<div class="title">
+                    <h1 class="heading"> About us </h1>
+				</div>
+				<div class="content">
+                <p>Wanderu is a travel search platform that helps travelers find the best deals on bus and
+                         train tickets across North America and Europe. With just a quick search on the Wanderu 
+                         website or the Wanderu app for iOS and Android, you can compare bus and train schedules
+                          and prices from hundreds of travel companies in one convenient place to find the bus or 
+                          train that works best for you. Wanderu’s simple checkout process makes it easy to book 
+                          your bus and train tickets hassle-free and with no additional fees.
+                        </p><br><br>
+					<div class="button">
+						<a href="">Read More</a>
+					</div>
+				</div>
+				<div class="social">
+					<a href=""><i class="fab fa-facebook-f"></i></a>
+					<a href=""><i class="fab fa-twitter"></i></a>
+					<a href=""><i class="fab fa-instagram"></i></a>
+				</div>
+			</div>
+			<div class="image-section">
+				<img src="img_one.jpg">
+			</div>
+            
+		</div>
+	</div>
+    
+
+<section class="footer">
+
+    <div class="box-container">
+
+        <div class="box" data-aos="fade-up">
+            <h3>our branches</h3>
+            <a href="#"> <i class="fas fa-map-marker-alt"></i> india </a>
+            <a href="#"> <i class="fas fa-map-marker-alt"></i> russia </a>
+            <a href="#"> <i class="fas fa-map-marker-alt"></i> japan </a>
+            <a href="#"> <i class="fas fa-map-marker-alt"></i> france </a>
+            <a href="#"> <i class="fas fa-map-marker-alt"></i> canada </a>
+        </div>
+
+        <div class="box" data-aos="fade-up">
+            <h3>quick links</h3>
+            <a href="#"> <i class="fas fa-chevron-right"></i> home </a>
+            <a href="#"> <i class="fas fa-chevron-right"></i> buses </a>
+            <a href="#"> <i class="fas fa-chevron-right"></i> trains </a>
+            <a href="#"> <i class="fas fa-chevron-right"></i>  my tickets</a>
+            <a href="#"> <i class="fas fa-chevron-right"></i> about us </a>
+            <a href="#"> <i class="fas fa-chevron-right"></i> contact us </a>
+            <a href="#"> <i class="fas fa-chevron-right"></i> blogs </a>
+        </div>
+
+        <div class="box" data-aos="fade-up">
+            <h3>contact info</h3>
+            <a href="#"> <i class="fas fa-phone"></i> +123-456-7890 </a>
+            <a href="#"> <i class="fas fa-phone"></i> +111-222-3333 </a>
+            <a href="#"> <i class="fas fa-envelop"></i> janhvi_harsh@gmail.com </a>
+            <a href="#"> <i class="fas fa-map-marker-alt"></i> pune, india - 411033 </a>
+        </div>
+
+        <div class="box" data-aos="fade-up">
+            <h3>follow us</h3>
+            <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
+            <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
+            <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
+            <a href="#"> <i class="fab fa-linkedin"></i> linkedin </a>
+            <a href="#"> <i class="fab fa-pinterest"></i> pinInterest </a>
+        </div>
+
+    </div>
+
+    <div class="credit">  all rights reserved </div>
+    
+
+</section>
+
+<script src="home.js"></script>
+</body>
+</html>
